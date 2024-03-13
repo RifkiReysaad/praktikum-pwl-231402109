ffb4965d0c4050e9f14365c4a7d5bcaca2a4507c
rifki <rifkireysaad20@gmail.com>
2024-03-13 13:12:06 +0700

    Tugas 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-4 bg-base-300 text-base-content bottom-0 absolute ">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Rifki Reysaad Bangun</p>
+    </aside>
+  </footer>
